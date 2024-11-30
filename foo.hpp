eb8e466c81cccd9518f4d33e1095ba34cba5a5d4
48534 <48534@DESKTOP-SJ3QDS3>
2024-11-30 16:00:39 +0100

    Zrobiłem zadanie

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,36 @@
-#pragma once
-
-#include "Human.hpp"
+#ifndef FOO_HPP
+#define FOO_HPP
 
 #include <list>
+#include <string> // Zak³adamy, ¿e Human u¿ywa std::string do przechowywania danych
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+class Human
 {
-    // Twoja implementacja tutaj
-    return {};
+public:
+    void birthday(); // Zwiêksza pole age o 1
+    bool isMonster(
+        const std::string& animal) const; // Zwraca true, jeœli osoba nie lubi danego zwierzêcia
+};
+
+std::vector< char > foo(std::list< Human >& humans)
+{
+    std::vector< char > result;
+
+    for (auto& human : humans) {
+        human.birthday();
+    }
+
+    for (auto it = humans.rbegin(); it != humans.rend(); ++it) {
+        if (it->isMonster("pies") && it->isMonster("kot")) {
+            result.push_back('n');
+        }
+        else {
+            result.push_back('y');
+        }
+    }
+
+    return result;
 }
+
+#endif // FOO_HPP
